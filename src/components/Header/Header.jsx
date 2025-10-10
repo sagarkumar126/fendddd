1fb1752a538c2fcbad461544916bd02c8c96de08
sagar0906 <sagar0906@example.com>
2025-10-11 02:28:52 +0530

    Initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
   return (
     <div className="header">
+      <img src={assets.header_img} alt="Header" className="header-image" />
 
-
-        <div className="header-contents">
-
+      <div className="header-contents">
         <h2>Order Your Favourite Food Here</h2>
-        <p>Chhose diverse of Food and End Your Cravings</p>
-
+        <p>Choose diverse of Food and End Your Cravings</p>
         <button>View Menu</button>
-
-        </div>
-      
+      </div>
     </div>
   )
 }
